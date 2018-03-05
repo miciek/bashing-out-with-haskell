b1829e8022a08ca852e43206d801aad2e6773716
miciek <miciek@gmail.com>
2018-03-05 15:27:32 +0100

    Add lts resolver to the hello.hs script

--- hello.hs
+++ hello.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack --install-ghc runghc --package turtle
+-- stack --resolver lts-10.8 --install-ghc runghc --package turtle
 {-# LANGUAGE OverloadedStrings #-}
 
 import Turtle
